2f3c622dd3605e02a4a19b7da85f60f949863eeb
mthpiltz <matheus.piltz@ufms.br>
2024-11-12 21:24:23 -0400

    Validação e formulario e criptografia de senha

--- app/src/main/java/com/example/trabalho3/view/CadastrarUsuario.java
+++ app/src/main/java/com/example/trabalho3/view/CadastrarUsuario.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.example.trabalho3.R;
+import com.example.trabalho3.Util;
 import com.example.trabalho3.database.AppDatabase;
 import com.example.trabalho3.databinding.FragmentCadastrarUsuarioBinding;
 import com.example.trabalho3.entity.Usuario;
@@ -33,18 +35,29 @@ public class CadastrarUsuario extends Fragment {
         binding.btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = binding.email.getText().toString();
-                String nome = binding.nome.getText().toString();
-                String senha = binding.nome.getText().toString();
+                String email = binding.email.getText().toString().trim();;
+                String nome = binding.nome.getText().toString().trim();;
+                String senha = binding.senha.getText().toString().trim();;
+
+                if(nome.isEmpty() || senha.isEmpty()){
+                    Toast.makeText(getActivity(), "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(getActivity(), "Por favor, insira um email válido", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String senhaCriptografada = Util.hashSenha(senha);
 
                 Usuario usuario = new Usuario();
                 usuario.setEmail(email);
                 usuario.setNome(nome);
-                usuario.setSenha(senha);
+                usuario.setSenha(senhaCriptografada);
 
                 db.usuarioDao().insert(usuario);
                 Toast.makeText(getActivity(), "Usuario inserido", Toast.LENGTH_SHORT).show();
-
             }
         });
 
